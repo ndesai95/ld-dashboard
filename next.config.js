1c45f529f5d42ea457ecf0fb7eca9d1410ed5c3c
Nicholas Desai <nicholas.desai@upstart.com>
2023-12-13 13:18:53 -0500

    feat: initial commit with patina

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const { createPatinaNextPlugin } = require('@upstart/patina-design-system/plugins/nextjs')
+const withPatina = createPatinaNextPlugin({
+  /** Patina config */
+})
 
-module.exports = nextConfig
+module.exports = withPatina({
+  /** Next config */
+})
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import '@upstart/patina-design-system/css-reset' // <-------- DON'T FORGET THIS FIRST
+import {PatinaProvider} from '@upstart/patina-design-system'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({Component, pageProps}: AppProps) {
+  return <PatinaProvider><Component {...pageProps} /></PatinaProvider>
 }
